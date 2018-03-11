7b3386e418f4f9f36d591d9d2e3c996079055f82
edmlinus <34444435+edmlinus@users.noreply.github.com>
2018-03-11 23:26:46 +0900

    Create getStarted.py

---
+++ getStarted.py
@@ -0,0 +1,15 @@
+//print a message
+print ("Hello, Python!");
+
+//declare and initialize variables.
+count = 100          
+size   = 1000.0      
+name    = "Edmlinus"       
+
+print count
+print size
+print name
+
+//make multiple assignments
+a,b,c = 100,1000.0,"Edmlinus"
+
