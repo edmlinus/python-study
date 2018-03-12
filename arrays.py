d8f7b63cc2d6a9ab0ad00f23a32b4c80a88aedb0
LGNOTBOOK\Hanjun <edmlinus@snu.ac.kr>
2018-03-12 10:39:35 +0900

    make some notes

--- arrays.py
+++ arrays.py
@@ -8,3 +8,7 @@ a = 1
 b,c = 2,3
 print(a)
 b = c+a
+
+#write more lines of code.. make editions. careful not to screw up the performance of other parts of the code,
+#saving changes here will only change the state of codes stored in your local repository, not the origin.
+#you must first commit your changes, and then push it to origin, to change the codes at the origin.
